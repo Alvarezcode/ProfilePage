2d2a400522b2f1b42c11a970fb9d090ac343e50d
Dario Alvarez <apacheraider123@gmail.com>
2024-06-04 15:05:29 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var element= document.querySelector('#Todd');
+
+function hide(element){
+    element.remove();
+}
+
+var element=document.querySelector('#switch');
+
+function change(element){
+    element.change('Dig Doug');
+
+    
+}
